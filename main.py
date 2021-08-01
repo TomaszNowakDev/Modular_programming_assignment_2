5354061688a24089f7e3dcabdf7d3fa61dc00581
Tomasz Nowak <tomeknew1@gmail.com>
2021-08-01 21:56:42 +0100

    Functions over150() and money_for_bonus() created and called.

--- main.py
+++ main.py
@@ -16,12 +16,23 @@ def list_of_wages(days):
     return wages
 
 
+def over150(days, wages):
+    return [days[i] for i in range(len(days)) if wages[i] > 150]
+
+
+def money_for_bonus(money):
+    return sum(money) * .1
+
+
 def main():
     weeks = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     employee_name, employee_age = employee_details()
     print(employee_name, employee_age)
     wages_catch = list_of_wages(weeks)
-    print(wages_catch)
+    over150_a_day = over150(weeks, wages_catch)
+    print(f'Days with more than 150€ earned: {over150_a_day}')
+    required_money = money_for_bonus(wages_catch)
+    print(f'Money required for bonus {required_money:.2f}€.')
 
 
 if __name__ == '__main__':
